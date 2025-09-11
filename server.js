91ec42890ba5bc141bdcd12d8fbb36084dbcdd90
Hieu Nghia <nguyenhieunghia241020@gmail.com>
2025-09-11 11:40:34 +0700

    Fix html giao dien server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // ---- Load FAQ từ file Excel ----
